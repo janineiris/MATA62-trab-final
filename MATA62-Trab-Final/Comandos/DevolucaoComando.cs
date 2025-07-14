7968f34a44857abbf9385c5b6448fae7a9af8482
Gabriel Soares <gabrielsoaresm94@gmail.com>
2025-07-14 01:15:20 -0300

    feat(add-comandos): update DevolucaoComando

--- MATA62-Trab-Final/Comandos/DevolucaoComando.cs
+++ MATA62-Trab-Final/Comandos/DevolucaoComando.cs
@@ -8,19 +8,36 @@ public class DevolucaoComando : IComando
     {
         Comando = comando;
     }
-    
+
     public void Executar(string[] args)
     {
         if (args.Length < 2)
         {
-            GerenciadorMensagens.ImprimeErroComando(Comando,"necessário passar os parâmetros <codigoUsuario> <codigoLivro>");
+            GerenciadorMensagens.ImprimeErroComando(Comando, "necessário passar os parâmetros <codigoUsuario> <codigoLivro>");
             return;
         }
 
         string codigoUsuario = args[0];
         string codigoLivro = args[1];
 
-        // Lógica da devolução aqui
-        Console.WriteLine($"Usuário {codigoUsuario} devolveu o livro {codigoLivro}");
+        var repositorio = Repositorio.ObterInstancia();
+        var usuario = repositorio.BuscarUsuarioEmprestadorPorCodigo(codigoUsuario);
+
+        if (usuario is null)
+        {
+            GerenciadorMensagens.ImprimeRecursoNaoEncontrado("usuario");
+            return;
+        }
+
+        var emprestimo = usuario.ObtemEmprestimoLivro(codigoLivro);
+
+        if (emprestimo is null)
+        {
+            GerenciadorMensagens.ImprimeErroComando(Comando, "nenhum empréstimo ativo encontrado para este livro");
+            return;
+        }
+
+        emprestimo.Devolver();
+        GerenciadorMensagens.Imprime("Devolução realizada com sucesso!");
     }
 }
