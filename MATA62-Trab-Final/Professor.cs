21a7d04fefd3e4055564e595174834104420d73a
Gabriel Soares <gabrielsoaresm94@gmail.com>
2025-07-14 01:09:54 -0300

    fix(add-comandos): fix Professor

--- MATA62-Trab-Final/Professor.cs
+++ MATA62-Trab-Final/Professor.cs
@@ -5,13 +5,12 @@ public class Professor: UsuarioEmprestador, IEmprestador, IObservador
     public override bool IsAluno => false;
     public string CodigoUsuario => CodIdentificacao;
     
-    public int TempoEmprestimo { get; private set; } = 8;
-    public int LimiteEmprestimos { get; private set; } = -1;
-    
     public int ContadorNotificacoes { get; private set; } = 0;
     
     public Professor(string codigo, string nome)
     {
+        TempoEmprestimo = 8;
+        LimiteEmprestimos = -1;
         CodIdentificacao = codigo;
         Nome = nome;
         _regraEmprestimo = new RegraEmprestimoProfessor();
